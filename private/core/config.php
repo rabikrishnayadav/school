8a952ee3f0e4498f77bb8c1a2062e5a0977b4c65
Rabi Krishna Yadav <rabikumar23154@gmail.com>
2023-08-26 02:21:19 -0700

    bootstrap and fontawesome add

--- private/core/config.php
+++ private/core/config.php
@@ -1,2 +1,3 @@
 <?php
 
+define('ROOT', 'http://localhost/school/public');
--- private/views/home.view.php
+++ private/views/home.view.php
@@ -3,9 +3,15 @@
     <head>
         <meta charset="utf-8">
         <title>Home</title>
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/bootstrap.min.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/all.min.css"> -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
     <body>
-        <h1>This is home view</h1>
+        <div class="container-fluid">
+            <h1><i class="fa fa-plus"></i>This is home view</h1>
+        </div>
     
         
     </body>
